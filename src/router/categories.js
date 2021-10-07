8df1e04b54d3cd1207cd43a99e86dba5deaa2b3b
Anonymous <Anonymous>
2021-10-07 19:45:10 +0300

    add hw5

--- src/router/categories.js
+++ src/router/categories.js
@@ -1,45 +1,43 @@
 const categoriesRoutes = [
   {
     path: '/add/payment/',
+    component: () => import('@/components/Schedule')
+  },
+  {
+    path: 'Food?value=:value',
+    name: 'food',
     component: () => import('@/components/Schedule'),
-    children: [
-      {
-        path: 'Food?value=:value',
-        name: 'food',
-        component: () => import('@/components/Schedule'),
-        props: true
-      },
-      {
-        path: 'Food', // дубль чтобы можно было переходить на эти роуты напрямую
-        name: 'food',
-        component: () => import('@/components/Schedule'),
-        props: true
-      },
-      {
-        path: 'Transport?value=:value',
-        name: 'transport',
-        component: () => import('@/components/Schedule'),
-        props: true
-      },
-      {
-        path: 'Transport', // дубль чтобы можно было переходить на эти роуты напрямую
-        name: 'transport',
-        component: () => import('@/components/Schedule'),
-        props: true
-      },
-      {
-        path: 'Entertainment?value=:value',
-        name: 'entertainment',
-        component: () => import('@/components/Schedule'),
-        props: true
-      },
-      {
-        path: 'Entertainment', // дубль чтобы можно было переходить на эти роуты напрямую
-        name: 'entertainment',
-        component: () => import('@/components/Schedule'),
-        props: true
-      }
-    ]
+    props: true
+  },
+  {
+    path: 'Food', // дубль чтобы можно было переходить на эти роуты напрямую
+    name: 'food',
+    component: () => import('@/components/Schedule'),
+    props: true
+  },
+  {
+    path: 'Transport?value=:value',
+    name: 'transport',
+    component: () => import('@/components/Schedule'),
+    props: true
+  },
+  {
+    path: 'Transport', // дубль чтобы можно было переходить на эти роуты напрямую
+    name: 'transport',
+    component: () => import('@/components/Schedule'),
+    props: true
+  },
+  {
+    path: 'Entertainment?value=:value',
+    name: 'entertainment',
+    component: () => import('@/components/Schedule'),
+    props: true
+  },
+  {
+    path: 'Entertainment', // дубль чтобы можно было переходить на эти роуты напрямую
+    name: 'entertainment',
+    component: () => import('@/components/Schedule'),
+    props: true
   }
 ]
 
